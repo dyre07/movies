d4d6b7b8fcecc7b305371f205e1255e81a7222ee
Aura Lintang Alif Salsabila <auralintang77@gmail.com>
2017-05-14 22:55:41 +0700

    bismillah

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl204/movies/model/Source.java
@@ -0,0 +1,18 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl204.movies.model;
+
+import java.util.List;
+
+/**
+ * Created by Dyre on 5/14/2017.
+ */
+
+public class Source {
+    public String id;
+    public String title;
+    public String overview;
+    public String poster_path;
+    public String category;
+    public String language;
+    public String country;
+    public List<String> sortBysAvailable;
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl204/movies/model/SourcesResponse.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl204.movies.model;
+
+import java.util.List;
+
+/**
+ * Created by Dyre on 5/14/2017.
+ */
+
+public class SourcesResponse {
+
+    public String page;
+    public List<Source> results;
+}
